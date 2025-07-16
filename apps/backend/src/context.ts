7a579225058d362d56638ff90b4404da984bc88a
Shwetanshu13 <shwetanshusinha13@gmail.com>
2025-07-16 18:03:53 +0530

    log added

--- apps/backend/src/context.ts
+++ apps/backend/src/context.ts
@@ -34,6 +34,6 @@ export const createContext = async ({
     }
   }
   console.log("Context created with user:", user);
-  console.log("Cookies header received:", req.headers.cookie);
+  console.log("Cookies header received:", req.headers);
   return { user };
 };
