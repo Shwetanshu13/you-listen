6f04b826f7e920b9cbb0cf515c77b260931a9c23
Shwetanshu13 <shwetanshusinha13@gmail.com>
2025-07-16 16:07:53 +0530

    changed redis init

--- apps/backend/src/lib/redis.ts
+++ apps/backend/src/lib/redis.ts
@@ -1,10 +1,5 @@
 import Redis from "ioredis";
 
-let redis: Redis | undefined;
-
-if (process.env.REDIS_URL) {
-  redis = new Redis(process.env.REDIS_URL!, {
-    maxRetriesPerRequest: null,
-  });
-}
-export { redis };
+export const redis = new Redis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null,
+});
